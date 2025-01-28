d352cb0adef87978813feb580a2df9e8eb38a47a
James Aaron Erang <iaacornus.devel@gmail.com>
2025-01-28 15:08:07 +0800

    parser.py: initial commit on parser module

---
+++ src/utils/parser/parser.py
@@ -0,0 +1,35 @@
+#
+# This is a part of CLSU Biotechnology and Analytical Laboratory
+# website along with the BaCaDM Database associated with it.
+#
+# Copyright (C) 2025 James Aaron Erang <iaacornus.devel@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the licence, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see <http://www.gnu.org/licenses/>.
+#
+# Author: James Aaron Erang <iaacornus.devel@gmail.com>
+#
+
+class Parser:
+    def __init__(self) -> None:
+        pass
+
+    def prepare_data(self) -> None:
+        pass
+
+    def clean_data(self) -> None:
+        pass
+
+    def remove_null(self) -> None:
+        pass
+
