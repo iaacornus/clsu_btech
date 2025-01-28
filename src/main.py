f6d4b76ec59e80fed4eb9966108afea71b480ca5
James Aaron Erang <iaacornus.devel@gmail.com>
2025-01-28 14:32:51 +0800

    update comment headers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 #
+# This is a part of CLSU Biotechnology and Analytical Laboratory
+# website along with the BaCaDM Database associated with it.
+#
 # Copyright (C) 2025 James Aaron Erang <iaacornus.devel@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
@@ -15,3 +18,4 @@
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
 #
 # Author: James Aaron Erang <iaacornus.devel@gmail.com>
+#
